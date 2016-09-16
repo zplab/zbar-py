e1638ca7508771ff875cedadaa8602c9dd339c11
Zach Pincus <zpincus@gmail.com>
2016-09-15 23:17:00 -0500

    simpler iconv fix

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ import distutils.command
 import distutils.command.build_ext
 import distutils.core
 import distutils.errors
+import os
+import ctypes
+import ctypes.util
 
 SRCS = '''Source/zbar/decoder.c
 Source/zbar/decoder/code128.c
@@ -25,10 +28,20 @@ Source/zbar/symbol.c'''.split('\n')
 
 INCLUDE = 'Source', 'Source/zbar'
 
+def has_libc_iconv():
+    if os.name != 'posix':
+        return False
+    libc = ctypes.CDLL(ctypes.util.find_library('c'))
+    return hasattr(libc, 'iconv')
+
+# don't try to link to standalone iconv library if it's already in libc
+# (iconv is in glibc, but  on OS X one needs a stanalone libiconv)
+LIBS = [] if has_libc_iconv() else ['iconv']
+
 zbar = distutils.core.Extension('zbar._zbar',
-    sources = ['zbar/_zbar.c'] + SRCS,
-    include_dirs = INCLUDE,
-    define_macros = [
+    sources=['zbar/_zbar.c'] + SRCS,
+    include_dirs=INCLUDE,
+    define_macros=[
         ('ENABLE_QRCODE', None),
         ('ENABLE_EAN', None),
         ('ENABLE_I25', None),
@@ -38,38 +51,12 @@ zbar = distutils.core.Extension('zbar._zbar',
         ('HAVE_INTTYPES_H', None),
         ('ZBAR_VERSION_MAJOR', 0),
         ('ZBAR_VERSION_MINOR', 10),
-        ('NO_STATS', None)]
+        ('NO_STATS', None)],
+    libraries=LIBS
 )
 
-class BuildExt(distutils.command.build_ext.build_ext):
-    @staticmethod
-    def has_lib(compiler, libname):
-        """Return a boolean indicating whether a linker flag name is supported on
-        the specified compiler.
-        """
-        import tempfile
-        with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-            f.write('int main (int argc, char **argv) { return 0; }')
-            try:
-                compiler.link_executable(compiler.compile([f.name]), "aksdfjhawkdjlfhkasdjhfkljasdhfkjahsdf", libraries=[libname])
-            except (distutils.errors.CompileError, distutils.errors.LinkError):
-                return False
-        return True
-
-    """A custom build extension for adding compiler-specific options."""
-    def build_extensions(self):
-        ldflags = []
-        if self.has_lib(self.compiler, 'iconv'):
-            ldflags.append('-liconv')
-        for ext in self.extensions:
-            ext.extra_link_args = ldflags
-            distutils.command.build_ext.build_ext.build_extensions(self)
-
-distutils.core.setup(
-    name = 'zbar',
-    version = '1.0',
-    description = 'zbar package',
-    ext_modules = [zbar],
-    packages = ['zbar'],
-    cmdclass={'build_ext': BuildExt}
- )
+distutils.core.setup(name='zbar',
+        version='1.0',
+        description='zbar package',
+        ext_modules=[zbar],
+        packages=['zbar'])
